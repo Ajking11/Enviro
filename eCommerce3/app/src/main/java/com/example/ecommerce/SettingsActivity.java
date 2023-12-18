1c3dfae5244588b6d066e485aec62f5b4cea9dbf
Jay <Josh_666_999@hotmail.co.uk>
2023-12-18 01:58:09 +0000

    First Version - Works
    Added - SettingsActivity.java & activity_settings.xml

--- eCommerce3/app/src/main/java/com/example/ecommerce/SettingsActivity.java
+++ eCommerce3/app/src/main/java/com/example/ecommerce/SettingsActivity.java
@@ -1,4 +1,206 @@
 package com.example.ecommerce;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SettingsActivity {
-}
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ecommerce.HomeActivity;
+import com.example.ecommerce.Prevalent.Prevalent;
+import com.example.ecommerce.R;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private CircleImageView profileImageView;
+    private EditText fullNameEditText, userPhoneEditText, addressEditText;
+    private StorageReference storageProfilePictureRef;
+
+    private Uri imageUri;
+    private String myUrl = "";
+    private StorageTask uploadTask;
+
+    public String checker = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        storageProfilePictureRef = FirebaseStorage.getInstance().getReference().child("Profile pictures");
+
+        profileImageView = findViewById(R.id.settings_profile_image);
+        fullNameEditText = findViewById(R.id.settings_full_name);
+        userPhoneEditText = findViewById(R.id.settings_phone_number);
+        addressEditText = findViewById(R.id.settings_address);
+
+        userInfoSaved(profileImageView, fullNameEditText, userPhoneEditText, addressEditText);
+
+        findViewById(R.id.close_settings_btn).setOnClickListener(v -> finish());
+
+        findViewById(R.id.update_account_settings_btn).setOnClickListener(v -> {
+            if (checker.equals("clicked")) {
+                userInfoSaved(profileImageView, fullNameEditText, userPhoneEditText, addressEditText);
+            } else {
+                updateOnlyUserInfo();
+            }
+        });
+
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checker = "clicked";
+                pickImageFromGallery();
+            }
+        });
+    }
+
+    private void pickImageFromGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
+            profileImageView.setImageURI(imageUri);
+        }
+    }
+
+    private void updateOnlyUserInfo() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        HashMap<String, Object> userMap = new HashMap<>();
+        userMap.put("name", fullNameEditText.getText().toString());
+        userMap.put("address", addressEditText.getText().toString());
+        userMap.put("phoneOrder", userPhoneEditText.getText().toString());
+        ref.child(Prevalent.currentOnlineUser.getPhone()).updateChildren(userMap);
+
+        startActivity(new Intent(SettingsActivity.this, HomeActivity.class));
+        Toast.makeText(SettingsActivity.this, "Profile updated", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    private void userInfoSaved(CircleImageView profileImageView, EditText fullNameEditText, EditText userPhoneEditText, EditText addressEditText) {
+        String fullName = this.fullNameEditText.getText().toString().trim();
+        String address = this.addressEditText.getText().toString().trim();
+        String phone = this.userPhoneEditText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(fullName)) {
+            Toast.makeText(this, "Name is mandatory.", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(address)) {
+            Toast.makeText(this, "Address is mandatory.", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(this, "Phone number is mandatory.", Toast.LENGTH_SHORT).show();
+        } else if (checker.equals("clicked")) {
+            uploadImage();
+        }
+    }
+
+    private void uploadImage() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Update Profile");
+        progressDialog.setMessage("Please wait, updating account information");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        if (imageUri != null) {
+            final StorageReference fileRef = storageProfilePictureRef.child(Prevalent.currentOnlineUser.getPhone() + ".jpg");
+
+            uploadTask = fileRef.putFile(imageUri);
+
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return fileRef.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUrl = task.getResult();
+                        myUrl = downloadUrl.toString();
+
+                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users");
+
+                        HashMap<String, Object> userMap = new HashMap<>();
+                        userMap.put("name", fullNameEditText.getText().toString());
+                        userMap.put("address", addressEditText.getText().toString());
+                        userMap.put("phoneOrder", userPhoneEditText.getText().toString());
+                        userMap.put("image", myUrl);
+                        ref.child(Prevalent.currentOnlineUser.getPhone()).updateChildren(userMap);
+
+                        progressDialog.dismiss();
+
+                        startActivity(new Intent(SettingsActivity.this, SettingsActivity.class));
+                        Toast.makeText(SettingsActivity.this, "Profile updated", Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        progressDialog.dismiss();
+                        Toast.makeText(SettingsActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            progressDialog.dismiss();
+            Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+        private void userInfoDisplay(CircleImageView profileImageView, EditText fullNameEditText, EditText userPhoneEditText, EditText addressEditText) {
+            DatabaseReference UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(Prevalent.currentOnlineUser.getPhone());
+
+            UsersRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        if (snapshot.child("image").exists()) {
+                            String image = snapshot.child("image").getValue().toString();
+                            String name = snapshot.child("name").getValue().toString();
+                            String phone = snapshot.child("phone").getValue().toString();
+                            String address = snapshot.child("address").getValue().toString();
+
+                            Picasso.get().load(image).into(profileImageView);
+                            fullNameEditText.setText(name);
+                            userPhoneEditText.setText(phone);
+                            addressEditText.setText(address);
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+    }
